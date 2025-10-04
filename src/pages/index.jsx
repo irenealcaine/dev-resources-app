6e71cfd79c412790da250a565d3b74c318ea400c
Irene Alcaine <irenealcainealvarez@gmail.com>
2025-10-04 16:14:00 +0200

    add filter

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
 import { categories, subcategories, resources } from "../data/examples"
 import { title, subtitle } from "../components/primitives";
 import DefaultLayout from "../layouts/default";
 import { Card, CardHeader, CardBody, CardFooter } from "@heroui/card";
 import { Divider } from "@heroui/divider";
-import { Image } from "@heroui/image";
 import { Link } from "@heroui/link";
+import { Input } from "@heroui/input";
 
 export default function IndexPage() {
+  const [search, setSearch] = useState("");
+
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
           <h1 className={title({ color: "violet" })}>Dev resources</h1>
+          <Input
+            key="outside"
+            label="Buscar"
+            labelPlacement="outside"
+            type="text"
+            variant="bordered"
+            value={search}
+            onChange={e => setSearch(e.target.value)}
+          />
 
           {categories.map((category) => (
             <div key={category.id}>
@@ -22,32 +34,36 @@ export default function IndexPage() {
                 {subcategories
                   .filter((sub) => sub.categoryId === category.id)
                   .map((sub) => (
-                    <>
-                      <div key={sub.id} className="text-default-600">
-                        {sub.name}
-                      </div>
+                    <div key={sub.id}>
+                      <div className="text-default-600">{sub.name}</div>
                       <div>
                         {
-                          resources.filter((res) => res.subcategoryId === sub.id).map((res) => (
-
-                            <Card key={res.id} className="max-w-[400px]">
-                              <CardHeader className="flex gap-3">
-                                <p className="text-md">{res.title}</p>
-                              </CardHeader>
-                              <Divider />
-                              <CardBody>
-                                <p>{res.description}</p>
-                              </CardBody>
-                              <Divider />
-                              <CardFooter>
-                                <Link isExternal showAnchorIcon href={res.url}>
-                                  Link
-                                </Link>
-                              </CardFooter>
-                            </Card>
-                          ))
-                        }</div>
-                    </>
+                          resources
+                            .filter(
+                              (res) =>
+                                res.subcategoryId === sub.id &&
+                                res.title.toLowerCase().includes(search.toLowerCase())
+                            )
+                            .map((res) => (
+                              <Card key={res.id} className="max-w-[400px]">
+                                <CardHeader className="flex gap-3">
+                                  <p className="text-md">{res.title}</p>
+                                </CardHeader>
+                                <Divider />
+                                <CardBody>
+                                  <p>{res.description}</p>
+                                </CardBody>
+                                <Divider />
+                                <CardFooter>
+                                  <Link isExternal showAnchorIcon href={res.url}>
+                                    Link
+                                  </Link>
+                                </CardFooter>
+                              </Card>
+                            ))
+                        }
+                      </div>
+                    </div>
                   ))}
               </ul>
             </div>
@@ -56,9 +72,6 @@ export default function IndexPage() {
             Beautiful, fast and modern React UI library.
           </div>
         </div>
-
-
-
       </section>
     </DefaultLayout>
   );
